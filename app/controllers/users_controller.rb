92aa831bd652600e722af7f7c3331980fa2dce44
Rey David Cuevas <cuevas.vela@gmail.com>
2020-02-26 15:07:51 -0600

    changed the form to use _tags instead raw html

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,11 +11,10 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
-  private
-  
-  def user_params
-    params.require(:user).permit(:username, :email, :password)  
-  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
