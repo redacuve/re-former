88f94395bb89899ad5e861943b1780e4bda012b1
Rey David Cuevas <cuevas.vela@gmail.com>
2020-02-26 12:12:51 -0600

    added view to the new action in users controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    validates :username, presence: true
-    validates :name, presence: true
-    validates :password, presence: true
+  validates :username, presence: true
+  validates :name, presence: true
+  validates :password, presence: true
 end
